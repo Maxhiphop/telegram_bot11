2739fa68cde31980ea4403325e8d0beaa4f5170b
Maxhiphop <128901397+Maxhiphop@users.noreply.github.com>
2025-02-25 19:17:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,6 +144,7 @@ if __name__ == "__main__":
         logging.error(f"Ошибка при запуске бота: {e}")
 
 
+
 with open("README.md", "a", encoding="utf-8") as file:
     file.write("# telegram_bot11\n")
 
